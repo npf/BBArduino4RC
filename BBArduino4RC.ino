157e84348767661676a6a0abe1946073643d1e30
Pierre Neyron <pierre.neyron@imag.fr>
2015-12-30 15:56:52 +0100

    Add USB RC controller support
    USB RC controller (joystick) is handled by the python script to send
    commnands via the serial line of the Arduino

--- BBArduino4RC.ino
+++ BBArduino4RC.ino
@@ -1,8 +1,3 @@
-/*#
-* BBArduino4RC - arduino code
-* Copyright: Pierre Neyron <pierre.neyron@free.fr>
-* License: GNU GPL v3
-*/
 #include <Servo.h>
 int cmd = 0;
 int servo_count = 6;
@@ -64,7 +59,8 @@ void set_led_internal(int i, int v) {
   }
 }
 
-void set_servo(int i, int v) {
+void set_servo(int i, int n) {
+  int v = n * (servo_max[i] - servo_min[i]) / 32;
   servo_pos[i] = max(v, servo_min[i]);
   servo_pos[i] = min(v, servo_max[i]);
   servo[i].write(servo_pos[i]);
@@ -74,18 +70,12 @@ void set_servo(int i, int v) {
   Serial.println(servo_pos[i], DEC);
 } 
 
-void set_servo_inc(int i, int v_inc) {
-  int v = servo_pos[i] + v_inc;
-  set_servo(i, v);
-} 
-  
-void set_motor(int i, int v) {
+void set_motor(int i, int n) {
+  int v = n * (motor_max[i] - motor_min[i]) / 32;
   if (v < motor_min[i]) {
     if (motor_speed[i] < v) {
-  	  Serial.println("up");
       motor_speed[i] = motor_min[i];
     } else {
-  	  Serial.println("down");
       motor_speed[i] = 0;
     }
   } else {
@@ -99,21 +89,16 @@ void set_motor(int i, int v) {
   Serial.println(motor_speed[i], DEC);
 } 
   
-void set_motor_inc(int i, int v_inc) {
-  int v = motor_speed[i] + v_inc;
-  set_motor(i, v);
-} 
-
 void setup() {
   Serial.begin(9600);
   servo_count = 2;
-  setup_servo(0,3,90,15,170);
-  setup_servo(1,5,90,15,170);
-  led_count = 2;
-  setup_led(0,2,0,500);
-  setup_led(1,13,0,1000);
+  setup_servo(0,3,90,0,180);
+  setup_servo(1,5,90,0,180);
   motor_count = 1;
-  setup_motor(0,11,25,255);
+  setup_motor(0,6,30,255);
+  led_count = 1;
+  setup_led(0,13,0,0);
+  Serial.println("Setup completed");
 }
 
 void loop() {
@@ -130,36 +115,43 @@ void loop() {
   }
   if (Serial.available() > 0) {
     cmd = Serial.read();
-    switch (cmd) {
-      case 113: // q
-        led_state[0] = 1; 
+    switch (cmd>>5) {
+      case 0: // Y1
+        Serial.print("Y1: ");
+        Serial.print(cmd & 31, DEC);
+        Serial.print(" #");
+        Serial.println(cmd, BIN);
+        set_motor(0, cmd & 31);
         break;
-      case 119: // w
-        led_state[0] = 0; 
+      case 1: // X1
+        Serial.print("X1: ");
+        Serial.print(cmd & 31, DEC);
+        Serial.print(" #");
+        Serial.println(cmd, BIN);
         break;
-      case 97: // a
-        led_state[1] = 1; 
+      case 2: // Y2
+        Serial.print("Y2: ");
+        Serial.print(cmd & 31, DEC);
+        Serial.print(" #");
+        Serial.println(cmd, BIN);
+        set_servo(0, cmd & 31);
         break;
-      case 115: // s
-        led_state[1] = 0; 
+      case 3: // X2
+        Serial.print("X2: ");
+        Serial.print(cmd & 31, DEC);
+        Serial.print(" #");
+        Serial.println(cmd, BIN);
+        set_servo(1, cmd & 31);
         break;
-      case 122: // z
-        set_servo_inc(0,5);
-        break;
-      case 120: // x
-        set_servo_inc(0,-5);
-        break;
-      case 99: // c
-        set_servo_inc(1,5);
-        break;
-      case 118: // v
-        set_servo_inc(1,-5);
-        break;
-      case 100: // d
-        set_motor_inc(0,5);
-        break;
-      case 102: // f
-        set_motor_inc(0,-5);
+      case 4: // z+t
+        Serial.print("ZT: ");
+        led_state[0] = (cmd & 31) >> 4;
+        led_blink[0] = (cmd & 15) * 200;
+        Serial.print(led_state[0], DEC);
+        Serial.print(" % ");
+        Serial.print(led_blink[0], DEC);
+        Serial.print(" #");
+        Serial.println(cmd, BIN);
         break;
       default:
         Serial.print("Unknown command: ");
---
+++ BBArduino4RC.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+#
+# BBArduino4RC.py
+# Copyright: Pierre Neyron <pierre.neyron@free.fr>
+# Licence GNU GPL v3
+#
+import pygame
+import os
+from select import select
+from sys import stdin
+import serial
+
+precalibrated=True
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+ttypy = os.open("/dev/pts/5", os.O_RDWR)
+ttyser = os.open("/dev/pts/3", os.O_RDWR)
+mapping = [2,0,1,5,4,3] 
+
+def myprint(str):
+    #print str
+    os.write(ttypy, str + "\n")
+
+pygame.init()
+pygame.joystick.init()
+    
+joystick_count = pygame.joystick.get_count()
+if joystick_count < 0:
+    exit(0)
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+    
+myprint("Joystick name: {}".format(joystick.get_name()))
+        
+axes = joystick.get_numaxes()
+myprint("Number of axes: {}".format(axes))
+        
+axis = range(6)
+axis_max = range(6)
+axis_min = range(6)
+val_new = range(6)
+val_old = range(6)
+
+pygame.event.get() 
+
+if precalibrated:
+    axis_max[0] = 0.721649169922
+    axis_min[0] = -0.597961425781
+    axis_max[1] = 0.721649169922
+    axis_min[1] = -0.433013916016
+    axis_max[2] = 0.432983398438
+    axis_min[2] = -0.608276367188
+    axis_max[3] = 0.536071777344
+    axis_min[3] = -0.639190673828
+    axis_max[4] = 0.896881103516
+    axis_min[4] = -0.876312255859
+    axis_max[5] = 0.917510986328
+    axis_min[5] = -0.979400634766
+else:
+    key=''
+    while key == '':
+        pygame.event.get() 
+        for i in range( axes ):
+            axis[i] = joystick.get_axis(mapping[i])
+        myprint("X1,Y1 = {:>6.3f}, {:>6.3f}".format(axis[0],axis[1]))
+        myprint("X2,Y2 = {:>6.3f}, {:>6.3f}".format(axis[2],axis[3]))
+        myprint("Z,T = {:>6.3f}, {:>6.3f}".format(axis[4],axis[5]))
+        myprint("Hit 'enter' when you are in the MIN position for all axes")
+        rlist, _, _ = select([stdin], [], [], 1)
+        if rlist:
+            key = stdin.readline(1)
+    for i in range( axes ):
+        axis_min[i] =  joystick.get_axis( mapping[i] )
+    
+    key=''
+    while key == '':
+        pygame.event.get() 
+        for i in range( axes ):
+            axis[i] = joystick.get_axis(mapping[i])
+        myprint("X1,Y1 = {:>6.3f}, {:>6.3f}".format(axis[0],axis[1]))
+        myprint("X2,Y2 = {:>6.3f}, {:>6.3f}".format(axis[2],axis[3]))
+        myprint("Z,T = {:>6.3f}, {:>6.3f}".format(axis[4],axis[5]))
+        myprint("Hit 'enter' when you are in the MAX position for all axes")
+        rlist, _, _ = select([stdin], [], [], 1)
+        if rlist:
+            key = stdin.readline(1)
+    for i in range( axes ):
+        axis_max[i] =  joystick.get_axis( mapping[i] )
+    
+    myprint("="*80)
+    for i in range(axes):
+        myprint("   axis_min[{}] = {}".format(i, axis_min[i]))
+        myprint("   axis_max[{}] = {}".format(i, axis_max[i]))
+
+while True:
+    pygame.event.get() 
+    myprint("="*80)
+    for i in range( axes ):
+        axis[i] = joystick.get_axis( mapping[i] )
+        if ((axis_max[i] - axis_min[i]) == 0):
+            val_new[i] = 0;
+        else:
+            val_new[i] = int(round(32 * (axis[i] - axis_min[i]) / (axis_max[i] - axis_min[i])))
+            val_new[i] = max(val_new[i], 0)
+            val_new[i] = min(val_new[i], 31)
+    myprint("X1,Y1 = {:>6.3f} < {:>6.3f} <{:>6.3f}, {:>6.3f} < {:>6.3f} < {:>6.3f}".format(axis_min[0],axis[0],axis_max[0],axis_min[1],axis[1],axis_max[1]))
+    myprint("X2,Y2 = {:>6.3f} < {:>6.3f} <{:>6.3f}, {:>6.3f} < {:>6.3f} < {:>6.3f}".format(axis_min[2],axis[2],axis_max[2],axis_min[3],axis[3],axis_max[3]))
+    myprint("Z,T = {:>6.3f} < {:>6.3f} <{:>6.3f}, {:>6.3f} < {:>6.3f} < {:>6.3f}".format(axis_min[4],axis[4],axis_max[4],axis_min[5],axis[5],axis_max[5]))
+    myprint("X1,Y1 = {}, {}".format(val_new[0],val_new[1]))
+    myprint("X2,Y2 = {}, {}".format(val_new[2],val_new[3]))
+    myprint("Z,T = {}, {}".format(val_new[4],val_new[5]))
+    for i in range(4):
+        code = (i<<5) + val_new[i]
+        myprint("{:3}: {:3d} ({:08b})".format(i, code, code))
+        if (val_new[i] != val_old[i]):
+            ser.write(chr(code))
+    code = (4<<5) + ((val_new[5]>15)<<4) + val_new[4]/2
+    myprint("{:3}: {:3d} ({:08b})".format("4+5", code, code))
+    if (val_new[4] != val_old[4] or val_new[5] != val_old[5]):
+        ser.write(chr(code))
+    while (ser.inWaiting() > 0):
+        os.write(ttyser, ser.read(ser.inWaiting()))
+    for i in range( axes ):
+        val_old[i] = val_new[i]
+    pygame.time.wait(200)            
+
+pygame.quit ()
